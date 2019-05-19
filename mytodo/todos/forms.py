b95a95f419bb8a510f7d72b1fd9435360bc841fe
momentum1993 <93stw@naver.com>
2019-05-19 15:29:22 +0900

    forms.py DateTimeField edit
    forms.py DateTimeField  inputformat edit

--- mytodo/todos/forms.py
+++ mytodo/todos/forms.py
@@ -3,11 +3,11 @@ from django import forms
 class TodoForm(forms.Form):
     title = forms.CharField(max_length=200, widget=forms.Textarea)
     priority = forms.IntegerField()
-    due_date = forms.DateTimeField(input_formats = ["%Y-%m-%dT%H:%M:%S"],
+    due_date = forms.DateTimeField(input_formats = ["%Y-%m-%dT%H:%M"],
         widget = forms.DateTimeInput(
             attrs={
                 'type': 'datetime-local',
                 'class': 'form-control'},
-            format='%Y-%m-%dT%H:%M:%S'))
+            format='%Y-%m-%dT%H:%M'))
     sub_todos = forms.CharField(widget=forms.Textarea)
 
